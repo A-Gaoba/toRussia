de2986d180ff32a599c06dabeb540b01cd60222a
Abdulrahman Gaoba <88571592+AbdulrahmanGooba@users.noreply.github.com>
2023-01-01 00:57:32 +0500

    nav

--- main.js
+++ main.js
@@ -8,11 +8,17 @@
 // };
 // headerBg();
 
-const burger = document.getElementById('burger');
-const navLinks = document.querySelector('.nav-links');
+const burger = document.getElementById("burger");
+const navLinks = document.querySelector(".nav-links");
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('isActive');
-})
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("isActive");
+});
 
+const menu_btn = document.querySelector(".hamburger");
+const mobile_btn = document.querySelector(".mobile_nav");
 
+menu_btn.addEventListener("click", () => {
+  menu_btn.classList.toggle("is-active");
+  mobile_btn.classList.toggle("is-active");
+});
