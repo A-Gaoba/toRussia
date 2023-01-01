97905e0770520352f8fa260d1d0ca292cf74d2c4
Abdulrahman Gaoba <88571592+AbdulrahmanGooba@users.noreply.github.com>
2023-01-01 11:43:48 +0500

    navbar burger done

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-// const headerBg = () => {
-//   const header = document.querySelector(".js-header");
-//   window.addEventListener("scroll", () => {
-//     this.scrollY > 0
-//       ? header.classList.add("bg-reveal")
-//       : header.classList.remove("bg-reveal");
-//   });
-// };
-// headerBg();
+
 
 const burger = document.getElementById("burger");
 const navLinks = document.querySelector(".nav-links");
